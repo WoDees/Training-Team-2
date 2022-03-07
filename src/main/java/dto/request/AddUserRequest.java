0a554c93259639bea0cdd46e1cb75aebf742cf4c
WoDees <29857995e@gmail.com>
2022-03-07 09:21:17 +0200

    correction

--- src/main/java/dto/request/AddUserRequest.java
+++ src/main/java/dto/request/AddUserRequest.java
@@ -40,13 +40,6 @@ public class AddUserRequest {
 
     public void setUserId(Long userId) {
         this.userId = userId;
-      
-    public String getMail() {
-        return mail;
-    }
-
-    public void setMail(String mail) {
-        this.mail = mail;
     }
 
     public boolean isOnlineStatus() {
