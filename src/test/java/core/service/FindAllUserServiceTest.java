13eed90bc288f67ce71c61936b295aba7586f6ba
WoDees <29857995e@gmail.com>
2022-03-08 11:05:36 +0200

    FindAllUserServiceTest

---
+++ src/test/java/core/service/FindAllUserServiceTest.java
@@ -0,0 +1,53 @@
+package core.service;
+
+import domain.UserEntity;
+import dto.UserEntityDTO;
+import dto.response.FindAllUserResponse;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import repository.Repository;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class FindAllUserServiceTest {
+
+    @Mock
+    private Repository repository;
+
+    @InjectMocks
+    private FindAllUserService findAllUserService;
+
+    @Test
+    void shouldFindAllToDos() {
+        var returnResult = entities();
+        when(repository.findAll()).thenReturn(returnResult);
+
+        var result = findAllUserService.findAll();
+        var expectedResult = response();
+
+        assertEquals(expectedResult, result);
+    }
+
+    private List<UserEntity> entities() {
+        var returnEntity = new UserEntity();
+        returnEntity.setUserId(1L);
+        returnEntity.setNickname("TEST_NICKNAME");
+        returnEntity.setPassword("TEST_PASSWORD");
+        returnEntity.setMail("TEST_MAIL");
+        returnEntity.setOnlineStatus(true);
+
+        return List.of(returnEntity);
+    }
+
+    private FindAllUserResponse response() {
+        var dto = new UserEntityDTO(1L, "TEST_NICKNAME", "TEST_PASSWORD", "TEST_MAIL", true);
+        return new FindAllUserResponse((List.of(dto)));
+    }
+}
