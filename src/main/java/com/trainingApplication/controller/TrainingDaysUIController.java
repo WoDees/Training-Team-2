c8ad87738ad645c942be8630431a0441b1ab544e
priho <prihodjkokirill@gmail.com>
2022-05-18 14:43:58 +0300

    New UI Controller + HTML

---
+++ src/main/java/com/trainingApplication/controller/TrainingDaysUIController.java
@@ -0,0 +1,40 @@
+package com.trainingApplication.controller;
+
+import com.trainingApplication.core.service.AddTrainingDaysService;
+import com.trainingApplication.core.service.FindAllTrainingDaysService;
+import com.trainingApplication.dto.request.AddTrainingDaysRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@AllArgsConstructor
+@RequestMapping("/ui")
+public class TrainingDaysUIController {
+
+    private final FindAllTrainingDaysService findAllTrainingDaysService;
+    private final AddTrainingDaysService addTrainingDaysService;
+
+    @GetMapping("/findAllTrainingDays")
+    public String findAllTrainingDays(Model model) {
+        var response = findAllTrainingDaysService.findAll();
+        model.addAttribute("days", response.getTrainingDaysDTOS());
+        return "findAllTrainingDays";
+    }
+
+    @GetMapping("/addTrainingDay")
+    public String addDayView(Model model) {
+        model.addAttribute("day", new AddTrainingDaysRequest());
+        return "addTrainingDay";
+    }
+
+    @PostMapping("/addTrainingDay")
+    public String addTrainingDays(@ModelAttribute AddTrainingDaysRequest day) {
+        addTrainingDaysService.add(day);
+        return "redirect:/ui/findAllTrainingDays";
+    }
+}
