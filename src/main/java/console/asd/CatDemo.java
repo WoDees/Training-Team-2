6f0ee7f82b7271438fef353df0a1b9b0889e48c4
WoDees <29857995e@gmail.com>
2022-05-24 19:09:18 +0300

    asd

---
+++ src/main/java/console/asd/CatDemo.java
@@ -0,0 +1,13 @@
+package console.asd;
+
+public class CatDemo {
+
+    public static void main(String[] args) {
+        Cat cat = new Cat("suka123");
+
+
+
+        System.out.println(cat.getName());
+    }
+
+}
