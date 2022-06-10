f329ccc0c1e0c1183c6868cc54660429cd7a0496
priho <prihodjkokirill@gmail.com>
2022-06-10 21:53:58 +0300

    H2 Test

--- src/test/java/com/trainingApplication/controller/TrainingDaysControllerTestIT.java
+++ src/test/java/com/trainingApplication/controller/TrainingDaysControllerTestIT.java
@@ -1,5 +1,12 @@
 package com.trainingApplication.controller;
 
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseOperation;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DatabaseTearDown;
+import com.github.springtestdbunit.annotation.ExpectedDatabase;
+import com.github.springtestdbunit.assertion.DatabaseAssertionMode;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
@@ -8,10 +15,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -19,13 +28,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@TestExecutionListeners(listeners = {DependencyInjectionTestExecutionListener.class, DbUnitTestExecutionListener.class},
+        mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS)
 class TrainingDaysControllerTestIT {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
+    @DatabaseSetup(value = "classpath:dbunit/create-trainingDay-dataset.xml")
+    @ExpectedDatabase(value = "classpath:dbunit/create-trainingDay-expected.xml", assertionMode = DatabaseAssertionMode.NON_STRICT)
+    @DatabaseTearDown(value = "classpath:dbunit/create-trainingDay-teardown.xml", type = DatabaseOperation.DELETE_ALL)
     void shouldCreateTrainingDay() throws Exception {
         mockMvc.perform(post("/training_days")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -34,11 +47,24 @@ class TrainingDaysControllerTestIT {
                 .andExpect(jsonPath("$.createdTrainingDaysId").value("1"));
     }
 
+    @Test
+    @DatabaseSetup(value = "classpath:dbunit/find-trainingDay-dataset.xml")
+    @DatabaseTearDown(value = "classpath:dbunit/find-trainingDay-dataset.xml")
+    void shouldGetTrainingDayById() throws Exception {
+        mockMvc.perform(get("/training_days/1000")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(createTrainingDayJSON()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.trainingDays.id").value("1000"))
+                .andExpect(jsonPath("$.trainingDays.trainingDay").value("test day"))
+                .andExpect(jsonPath("$.trainingDays.description").value("test description"));
+    }
+
     private String createTrainingDayJSON() throws JSONException {
         return new JSONObject()
-                .put("trainingDate", "04/01/2000")
-                .put("description", "qwerty123")
-                .put("userId", 1)
+                .put("trainingDay", "test day")
+                .put("description", "test description")
+                .put("userId", "2000")
                 .toString();
     }
 }
--- src/test/java/test_factory/TestTrainingDaysDtoFactory.java
+++ src/test/java/test_factory/TestTrainingDaysDtoFactory.java
@@ -5,31 +5,18 @@ import com.trainingApplication.dto.request.AddTrainingDaysRequest;
 
 public class TestTrainingDaysDtoFactory {
 
-    public static AddTrainingDaysRequest createAddCalendarRequest() {
-        var request = new AddTrainingDaysRequest();
-        request.setTrainingDate("14/03/2022");
-        request.setDescription("Test_Description");
-        return request;
-    }
-
-    public static AddTrainingDaysRequest createRequestWithoutEventDate() {
-        var request = new AddTrainingDaysRequest();
-        request.setDescription("Test_Description");
-        return request;
-    }
-
     public static TrainingDaysEntity createEntity(Long id) {
         var entity = new TrainingDaysEntity();
         entity.setId(id);
         entity.setUserId(id);
-        entity.setTrainingDate("28/02/2022");
+        entity.setTrainingDay("28/02/2022");
         entity.setDescription("Test description");
         return entity;
     }
 
     public static AddTrainingDaysRequest createRequest() {
         var request = new AddTrainingDaysRequest();
-        request.setTrainingDate("28/02/2022");
+        request.setTrainingDay("28/02/2022");
         request.setDescription("Test description");
         return request;
     }
